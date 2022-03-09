7ac6217ad1c0a53ed2daa682e80a2aa57918488e
Liverino <olivia.victorino@gmail.com>
2022-03-09 04:40:06 -0800

    afhdisa

--- src/QuestionsForm.js
+++ src/QuestionsForm.js
@@ -21,7 +21,7 @@ class QuestionsForm extends React.Component {
         //console.log("fuck")
         try {
            // console.log("we got to the sumbit + " + this.state)
-            let response = fetch(https://personalsite-backend.herokuapp.com/posts",
+            let response = fetch("https://personalsite-backend.herokuapp.com/posts",
             
             {
                 method: "POST",
